65326d9fa40d19347fae6b7b4ffcfca82e57b5f8
mpayne30 <142361747+mpayne30@users.noreply.github.com>
2023-09-18 13:12:35 -0500

    Update LogicBoard.java
    Moved GPT to LogicBoard.java

--- LogicBoard.java
+++ LogicBoard.java
@@ -1,10 +1,95 @@
-public class LogicBoard {
-    private LogicPiece[][] board = new LogicPiece[8][8];
-    //going to leave this void for now, may want to make it boolean for if they deselect/ make a bad move
-    public void makeMove(LogicPiece startPiece, LogicPiece endLocation){
-        if (isValidMove(startPiece, endLocation)){
-            this.board[endLocation.getX()][endLocation.getY()] = new LogicPiece(startPiece, false);
-            this.board[startPiece.getX()][startPiece.getY()] = new LogicPiece(endLocation,  true);
+package HelperFiles;
+//JTO: This may prove helpful to get you started
+
+import java.util.Scanner;
+
+public class CheckersGamebyGPT {
+    private static LogicPiece[][] board;
+    private static final int BOARD_SIZE = 8;
+    private static final char EMPTY_CELL = '-';
+    private static final char PLAYER_1_PIECE = 'X';
+    private static final char PLAYER_2_PIECE = 'O';
+
+    public static void main(String[] args) {
+        initializeBoard();
+        boolean player1Turn = true;
+        boolean gameIsRunning = true;
+
+        while (gameIsRunning) {
+            printBoard();
+            char currentPlayerPiece = player1Turn ? PLAYER_1_PIECE : PLAYER_2_PIECE;
+            System.out.println("Player " + (player1Turn ? "1" : "2") + "'s turn (" + currentPlayerPiece + ")");
+            int[] move = getPlayerMove();
+            int startX = move[0];
+            int startY = move[1];
+            int endX = move[2];
+            int endY = move[3];
+
+            if (isValidMove(startX, startY, endX, endY, currentPlayerPiece)) {
+                makeMove(startX, startY, endX, endY, currentPlayerPiece);
+                player1Turn = !player1Turn;
+            } else {
+                System.out.println("Invalid move! Try again.");
+            }
+
+            // Check for game over conditions (e.g., one player has no pieces left)
+            // Implement this logic according to the rules of checkers.
+
+            // You can add AI logic here to make the computer player's move.
         }
+
+        System.out.println("Game over!");
+    }
+
+    private static void initializeBoard() {
+        board = new LogicPiece[BOARD_SIZE][BOARD_SIZE];  //use whatever class the checkers pieces are
+
+        for (int x = 0; x < BOARD_SIZE; x++) {
+            for (int y = 0; y < BOARD_SIZE; y++) {
+                if ((x + y) % 2 != 0) {
+                    board[x][y] = new LogicPiece(x, y, "empty");      //this piece should be effectively null and wont be visible
+                } else if (y <= 2) {
+                    board[x][y] = new LogicPiece(x, y, "player1");      //call piece constructor with either player 1 parameter or player 2 parameter (should use colors once we know what they are)
+                } else if (y >= BOARD_SIZE - 3) {
+                    board[x][y] = new LogicPiece(x, y, "player2");      //call piece constructor with other player parameter
+                } else {
+                    board[x][y] = new LogicPiece(x, y, "empty");      //this piece should be effectively null and wont be visible
+                }
+            }
+        }
+    }
+
+    private static void printBoard() {
+        System.out.println("  0 1 2 3 4 5 6 7");
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[] getPlayerMove() {
+        Scanner scanner = new Scanner(System.in);
+        int[] move = new int[4];
+        System.out.print("Enter your move (startX startY endX endY): ");
+        for (int i = 0; i < 4; i++) {
+            move[i] = scanner.nextInt();
+        }
+        return move;
+    }
+
+    private static boolean isValidMove(int startX, int startY, int endX, int endY, char currentPlayerPiece) {
+        // Implement logic to check if the move is valid according to the rules of checkers.
+        // This includes checking if the move is within the bounds of the board, if it's a diagonal move, and more.
+        // You'll also need to check if the move is allowed for regular pieces and kings.
+        return false; // Placeholder, replace with your logic.
+    }
+
+    private static void makeMove(int startX, int startY, int endX, int endY, char currentPlayerPiece) {
+        // Implement logic to update the board after a valid move.
+        // This includes moving the piece, capturing opponent pieces, and promoting to a king if applicable.
+        // You'll also need to handle multiple captures if available.
     }
 }
