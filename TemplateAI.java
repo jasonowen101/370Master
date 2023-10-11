201adcf1fc15118e114fbfcbc6e6fac3a3bbf028
cwilson80 <cwilson80@murraystate.edu>
2023-10-11 13:38:04 -0500

    Added basic implementation

--- TemplateAI.java
+++ TemplateAI.java
@@ -1,3 +1,9 @@
-public interface TemplateAI {
-    
+public interface TemplateAI<T> {
+    /**
+     * Function to calculate and return the move that the AI wants to perform.
+     * T is a placeholder value for the move the AI will perform. In the end it should not use a generic type.
+     *
+     * @return The move that the AI has decided to make.
+     */
+    public T getMove();
 }
