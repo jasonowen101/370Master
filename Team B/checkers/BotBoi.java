c16f8b2d4f4c12ce4031baff25b01f913bcb6502
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2023-10-18 13:42:23 -0500

    created a class to interact with game

---
+++ Team B/checkers/BotBoi.java
@@ -0,0 +1,26 @@
+package checkers;
+import java.awt.Color;
+
+public class BotBoi {
+    //aight, sooo this class is going to take in game data and select the best move (prolly not the best).
+    //the game should create an object of BotBoi which will be a robot player. 
+    private Color team;         //right now teams are colors but this can be changed
+    private CheckerSquare[][] pieces = GamePanel.getSquares();      //this assigns a name to the squares array for shorter name in this class
+
+    //construct a bot and assign it a team
+    public BotBoi(Color team) {
+        this.team = team;
+    }
+
+    //The game should call bot.getMove(), and the bot should return its move as an array of 2 CheckerSquares
+    public CheckerSquare[] getMove() {
+        CheckerSquare[] move = new CheckerSquare[] {pieces[0][0], pieces[1][1]};  
+        //^^^ initialized because i dont like red squiggles ^^^
+
+        //function call to the algorithm that actually determines the best move (or any move as long as it works lmao) should go here
+        //ALGORITHM SHOULD RETURN MOVE AS AN ARRAY OF CHECKERSQUARES (size of 2)
+        //move = determineBestMove()     (or whatever the function is called)
+
+        return move;
+    }
+}
