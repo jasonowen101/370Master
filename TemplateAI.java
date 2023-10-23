e1fdd25e7215b9ea57b234b6f47e9fc8540c39fe
cwilson80 <cwilson80@murraystate.edu>
2023-10-23 14:09:25 -0500

    Finish interface

--- TemplateAI.java
+++
@@ -1,9 +0,0 @@
-public interface TemplateAI<T> {
-    /**
-     * Function to calculate and return the move that the AI wants to perform.
-     * T is a placeholder value for the move the AI will perform. In the end it should not use a generic type.
-     *
-     * @return The move that the AI has decided to make.
-     */
-    public T getMove();
-}
