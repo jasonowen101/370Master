37707be17e3ac6e11da7761a543ca7366de77694
rakers18 <drewrakers@icloud.com>
2023-10-23 13:30:48 -0500

    Update EndScreens.java

--- Team B/checkers/EndScreens.java
+++ Team B/checkers/EndScreens.java
@@ -1,3 +1,11 @@
-public class EndScreens {
+interface EndScreens {
     
+    void displayWhoWon();
+    void restartButton();
+    void quitButton();
+    
+
 }
+
+//Create an interface for the end screen
+//only difference is "Blue Wins", "Yellow Wins", and "Draw"
