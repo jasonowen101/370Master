44370090d9fb85432a8a132b488f860ec33d1d12
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2023-10-24 12:32:45 -0500

    comments

--- Team B/checkers/GUI.java
+++ Team B/checkers/GUI.java
@@ -7,11 +7,11 @@ public class GUI {
     public static boolean gameOver = true;
     public static int drawMoves = 0; //for check end
     public static boolean blueTurn;
-    public static BotBoi jarvis;
+    public static BotBoi jarvis;     //Jarvis will die...he's just here to see if things work
     public static void main(String[] args){
         new Ctegame(); //Created an instance of Ctegame
         
-        //this loop waits for a game mode to be selected before entering game loop
+        //this loop waits for a game mode to be selected and for game to start before entering game loop
         while(gameMode == null || gameOver){
             try{
                 Thread.sleep(100);
@@ -35,12 +35,12 @@ public class GUI {
                 }                                                       //      this check is mainly for the human players
                 updateBoard(move);      //updates game board state and graphics
                 blueTurn = !blueTurn;   //changing of turns
-                if(blueTurn){
+                if(blueTurn){           //this just displays whose turn it is
                     GamePanel.setTurnLabelText("Blue's turn");
                 } else {
                     GamePanel.setTurnLabelText("Yellow's turn");
                 }
-                gameOver = checkGameOver();
+                gameOver = checkGameOver();     //check to see if anybody has won
             }
         }
 
@@ -48,34 +48,41 @@ public class GUI {
 
     private static CheckerSquare[] nextMove(){
         CheckerSquare[] move;
-        if(gameMode == "pvp") {
+        if(gameMode == "pvp") {         //human v human game mode
             return getMoveFromMouse();
         }
-        if(gameMode == "pvc") {
+        if(gameMode == "pvc") {         //human v pc game mode
             if(!blueTurn){
                 move = jarvis.getMove();   //bot makes move here
+                move[0].setSelected(true);      //"animation" of bot move starts here
+                try{                
+                    Thread.sleep(2000);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+                 move[0].setSelected(false);
                 return move;
             } else {
-                return getMoveFromMouse();
+                return getMoveFromMouse();  //player makes move here if it's their turn
             }
         }
-        if(gameMode == "cvc") {
+        if(gameMode == "cvc") {         //This will work just like pvc but with two bots
             return null;
         }
         return null;
     }
     
-    private static CheckerSquare[] getMoveFromMouse() {
-        CheckerSquareMouse.active = true;
-        while(CheckerSquareMouse.getMove() == null) {
+    private static CheckerSquare[] getMoveFromMouse() { //this function kinda scuffed but it works for now....if any one has an idea of how to make better lmk
+        CheckerSquareMouse.active = true;               //activates the Mouse adapter
+        while(CheckerSquareMouse.getMove() == null) {   //continually checks if the player has input a move
             try{
                 Thread.sleep(75);
             } catch(InterruptedException exception) {
                 exception.printStackTrace();
             }
         }
-        CheckerSquare[] move;
-        move = CheckerSquareMouse.getMove();
+        CheckerSquare[] move;                           //in order to be able to clear the move from mouse and still be able to use it
+        move = CheckerSquareMouse.getMove();            //  I used a place holder move
         CheckerSquareMouse.clearMove();
         CheckerSquareMouse.active = false;
         return move;
@@ -83,11 +90,11 @@ public class GUI {
     
     //This method updates the game board state as well as the board graphics 
     private static void updateBoard(CheckerSquare[] move){
-        move[1].toggleChecker(move[0].getCheckerColor());
-        move[0].toggleChecker(null);
+        move[1].toggleChecker(move[0].getCheckerColor());       //this toggles the square where piece is going 
+        move[0].toggleChecker(null);                      //this toggles the square where piece came from....jumped pieces are toggled in MoveValidator isValidMove()
     }
 
-    //Call everywhere checkPromote is called
+    //Call everywhere checkPromote is called....I actually just call it at the end of every game loop iteration 
     public static boolean checkGameOver(){
         int activeBluePiece = 0;
         int activeYellowPiece = 0;
@@ -104,26 +111,26 @@ public class GUI {
 
         if (activeBluePiece == 0 && drawMoves <= 40){
             // Call win panel with blue as victor
-            GamePanel.setTurnLabelText("Blue Wins!");
+            GamePanel.setTurnLabelText("Blue Wins!");       //This will do for now but a victory panel would be nice
             return true;
         } else if (activeYellowPiece == 0 && drawMoves <= 40) {
             // Call win panel with yellow as victor
-            GamePanel.setTurnLabelText("Yellow Wins!");
+            GamePanel.setTurnLabelText("Yellow Wins!");     //^^^^
             return true;
         } else if (drawMoves > 40){
             // Call draw panel
-            GamePanel.setTurnLabelText("It's a draw!");
+            GamePanel.setTurnLabelText("It's a draw!");     //^^^^^
             return true;
         }
         return false;
     }
 
-    // Call after every jump
+    // Call after every jump...right now this is called in MoveValidator isValidMove();
     public static void pieceJumped(){
         drawMoves = 0;
     }
 
-    // Call after every move that is not a jump
+    // Call after every move that is not a jump....called in checkPromote of MoveValidator b/c checkPromote called before every valid move
     public static void pieceMoved(){
         drawMoves = drawMoves + 1;
     }
