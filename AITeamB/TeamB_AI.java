28d844d7d8127b0ea27fc03dd9e93b8ac254831c
JElwellMSU <jelwell@murraystate.edu>
2023-10-25 15:04:53 -0400

    Started Valuate Method

--- AITeamB/TeamB_AI.java
+++ AITeamB/TeamB_AI.java
@@ -28,7 +28,7 @@ public class TeamB_AI {
         for(LogicPiece piece : boardState) //needs logic piece to be done first
         {
             Move move;
-            move = valuate(piece);
+            move = valuate(piece); // assign best move to piece
             allMoves.put(move.score, move);
         }
 
@@ -45,6 +45,101 @@ public class TeamB_AI {
         
     }
 
-    //public void executeMove(Move move)
+    // returns highest score move to be executed
+    public static Move executeMove(Move move)
+    {
+        return move;
+    }
 
+    // find best move for a piece
+    // each check will have a score and if the score is higher than the previous score, it will be the new score and setPieceTo will be set to the move that gave that score
+    public static Move valuate(LogicPiece piece)
+    {
+        Move move = new Move();
+        move.setPieceFrom(piece.getX(), piece.getY());
+        move.setPieceTo(piece.getX(), piece.getY());;
+        move.score = 0;
+
+        // check if its a king, if so add in reverse checks
+        // going to finish the rest of the checks for a normal piece first and then add in the king checks (should be almost a copy paste job)
+
+        // check if it can jump
+        // checks if it can jump to the right by checking if the piece to the right is an opponent and if the piece to the right of that is empty
+        // need to check if the piece is in bounds too
+        if(piece.getX()+2 < 8  && piece.getY()+2 < 8 && boardState[piece.getX()+1][piece.getY()+1] != null &&  piece.getTeam() != boardState[piece.getX()+1][piece.getY()+1].getTeam && boardState[piece.getX()+2][piece.getY()+2] == null)
+        {
+            if(move.getScore() < 3)
+            {
+                move.setPieceTo(piece.getX()+2, piece.getY()+2);
+                move.setScore(3);
+                if(doesKing(piece)) // check if the jump would also make it a king
+                {
+                    move.setScore(5);
+                }
+            }
+        }
+        // same as before but for the left
+        if(piece.getX()-2 > -1  && piece.getY()+2 < 8 && boardState[piece.getX()-1][piece.getY()+1] != null &&  piece.getTeam() != boardState[piece.getX()-1][piece.getY()+1].getTeam && boardState[piece.getX()-2][piece.getY()+2] == null)
+        {
+            if(move.getScore() < 3)
+            {
+                move.setPieceTo(piece.getX()-2, piece.getY()+2);
+                move.setScore(3);
+                if(doesKing(piece)) // check if the jump would also make it a king
+                {
+                    move.setScore(5);
+                }
+            }
+        }
+        // check if it can become a king by left move
+        if(piece.getX()-1 > -1  && piece.getY()+1 == 7 && boardState[piece.getX()-1][piece.getY()+1] == null)
+        {
+            if(move.getScore() < 2)
+            {
+                move.setPieceTo(piece.getX()-1, piece.getY()+1);
+                move.setScore(2);
+            }
+        }
+        // check if it can become a king by right move
+        if(piece.getX()+1 > 8  && piece.getY()+1 == 7 && boardState[piece.getX()-1][piece.getY()+1] == null)
+        {
+            if(move.getScore() < 2)
+            {
+                move.setPieceTo(piece.getX()+1, piece.getY()+1);
+                move.setScore(2);
+            }
+        }
+        // check if it can block an opponent to the left
+        // check if it can block an opponent to the right
+
+        // check if it can move left
+        if(piece.getX()-1 > -1  && piece.getY()+1 < 8 && boardState[piece.getX()-1][piece.getY()+1] == null)
+        {
+            if(move.getScore() < 1)
+            {
+                move.setPieceTo(piece.getX()-1, piece.getY()+1);
+                move.setScore(1);
+            }
+        }
+        // check if it can move right
+        if(piece.getX()+1 > 8  && piece.getY()+1 < 8 && boardState[piece.getX()-1][piece.getY()+1] == null)
+        {
+            if(move.getScore() < 1)
+            {
+                move.setPieceTo(piece.getX()+1, piece.getY()+1);
+                move.setScore(1);
+            }
+        }
+        // check if it can be jumped
+
+        return move;
+    }
+    
+    public boolean doesKing(LogicPiece piece)
+    {
+        if(piece.getY() == 7)
+        {
+            return true;
+        }
+    }
 }
