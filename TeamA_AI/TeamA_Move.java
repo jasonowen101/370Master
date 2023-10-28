0b0c926f31d48cd729472395c25088dd63bef641
unknown <nathan.d.borchelt@gmail.com>
2023-10-28 13:24:56 -0500

    more AI, starting move development

---
+++ TeamA_AI/TeamA_Move.java
@@ -0,0 +1,17 @@
+package TeamA_AI;
+
+import checkers.CheckerSquare;
+
+public class TeamA_Move {
+    private CheckerSquare original;
+    private CheckerSquare end;
+    private short score;
+
+    public TeamA_Move(CheckerSquare original, CheckerSquare end){
+        this.original = original;
+        this.end = end;
+
+        if
+        if(original.getCheckerColor() == CheckerSquare.TEAM2)
+    }
+}
