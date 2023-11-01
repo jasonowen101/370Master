22e9bef21073fe0eb4ded55a5746af8bbae4e31b
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2023-11-01 12:07:27 -0500

    comments

--- Team B/checkers/Ctegame.java
+++ Team B/checkers/Ctegame.java
@@ -15,13 +15,14 @@ public class Ctegame extends JFrame {
     static HelpPanel hp = new HelpPanel();
     static GamePanel gp = new GamePanel();
 
-    static splashscreen splash = new splashscreen(); // Adding the SplashScreen
+    static splashscreen splash = new splashscreen(); //this is the splashscreen panel that displays on game launch
 
-    static CardLayout cl = new CardLayout();
-    static BackgroundPanel cards;  // to contain the panels as cards
+    static CardLayout cl = new CardLayout();        //this layout contains the different panels (menu, game, etc.)
+    static BackgroundPanel cards;                   //to contain the panels as cards
     
     Ctegame()
     {
+        //This creates an image that will be used as the background to the game
         Image img;
         try{
             img = ImageIO.read(new URL("https://drive.google.com/uc?id=17X4N_GoW9BAyFgYueQHDUFTvz5GwweyC"));
@@ -30,7 +31,8 @@ public class Ctegame extends JFrame {
             img = null;
         }  
         
-        cards =  new BackgroundPanel(img);
+        cards =  new BackgroundPanel(img);  //This is the main panel that is displayed by the frame. 
+                                            //It holds other panels which can be displayed 1 at a time using cl.show()
 
         cards.setLayout(cl); // setting the layout to cardlayout
         cards.add(splash, "SplashScreen"); // Addding the SplashScreen as a card
@@ -42,13 +44,13 @@ public class Ctegame extends JFrame {
         setTitle("Checkers");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(720, 720);  //frame size
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(null);   //frame loads in center of screen
         setVisible(true);   // frame visibility
 
         // Initially showing the SplashScreen
         cl.show(cards, "SplashScreen");
 
-        // Started a timer to transition from the SplashScreen to the MenuPanel after 3 seconds
+        // Started a timer to transition from the splashscreen to the MenuPanel after 3 seconds
         Timer timer = new Timer(5000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
