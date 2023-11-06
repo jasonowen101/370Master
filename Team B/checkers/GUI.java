29af0f611ffe996f38f62d8235f1ed202539d626
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2023-11-06 11:15:50 -0600

    comments and set up for bot v bot play

--- Team B/checkers/GUI.java
+++ Team B/checkers/GUI.java
@@ -3,49 +3,47 @@ package checkers;
 import java.awt.Color;
 
 public class GUI {
-    public static String gameMode = null;
-    public static boolean gameOver = true;
-    public static int drawMoves = 0; //for check end
-    public static boolean blueTurn;
-    public static BotBoi jarvis;     //Jarvis will die =(...he's just here to see if things work
+    public static String gameMode = null;   // 3 possible values, "pvp" (person vs person) "pvc" (person vs computer) "cvc" (computer vs computer)
+    public static boolean gameOver = true;  //game is over when gameOver = true
+    public static int drawMoves = 0;        //for check end
+    public static boolean blueTurn;         //its blue's turn when blueTurn = true
+    public static BotBoi jarvis;            //this is where you can initialize bots...Jarvis will die =(...he's just here to see if things work
     public static void main(String[] args){
-        new Ctegame(); //Created an instance of Ctegame
+        new Ctegame(); //Created an instance of Ctegame, which constructs the GUI
         
         /* this loop waits for a game mode to be selected and for game to start before entering game loop...
         The reasoning for sleeping the thread is so the while loop doesn't bang out condition checks as fast
         as possible and get stuck or slow things down.*/
-        
         while(gameMode == null || gameOver){
             try{
                 Thread.sleep(100);
                 //waiting for game mode...we have a delay so the machine doesn't kill itself checking the while condition
-            } catch(InterruptedException exception) {
-                exception.printStackTrace();
+            } catch(InterruptedException e) {
+                e.printStackTrace();
             }
         } 
 
         
-        //create bots here
+        //CREATE BOTS HERE
         jarvis = new BotBoi(Color.YELLOW);
 
-        CheckerSquare[] move = new CheckerSquare[2];
+        CheckerSquare[] move = new CheckerSquare[2];  //The way our game works, a move is represented by an array containing 2 squares, the start and end respectively
         //GAME LOOP 
         while(true) {
-            if(!gameOver) {
+            if(!gameOver) {     //This block runs the game while the game isn't over
                 move = nextMove();      //gets a move based on game mode.....also for the bots, checkerSelected should be updated in nextMove for graphics so we can tell whats going on
                 while(!MoveValidator.isValidMove(blueTurn, move)){      //check if the move is valid and if not, get a new one
                     move = nextMove();                                  //^^^The AIs should not rely on this check and should validate their own moves with the same method...in other words
                 }                                                       //      this check is mainly for the human players
-                updateBoard(move);      //updates game board state and graphics
-                blueTurn = !blueTurn;   //changing of turns
-                if(blueTurn){           //this just displays whose turn it is
+                updateBoard(move);      //updates game board data and GUI graphics
+                blueTurn = !blueTurn;   //changing of turns after a move has been made
+                if(blueTurn){           //this just displays whose turn it is in a jlabel
                     GamePanel.setTurnLabelText("Blue's turn");
                 } else {
                     GamePanel.setTurnLabelText("Yellow's turn");
                 }
                 gameOver = checkGameOver();     //check to see if anybody has won
-            } else{
-                //do something to reset game  (might need to sleep thread)
+            } else{     //when the game is over, continually check to see if game has restarted (on a delay so loop doesn't bang out checks and break)
                 try{
                     Thread.sleep(500);
                 } catch(InterruptedException e){
@@ -53,17 +51,16 @@ public class GUI {
                 }
             }
         }
-
     } //end of main()
 
-    private static CheckerSquare[] nextMove(){
-        CheckerSquare[] move;
+    private static CheckerSquare[] nextMove(){  //very important method
+        CheckerSquare[] move = null;
         if(gameMode == "pvp") {         //human v human game mode
             return getMoveFromMouse();
         }
-        if(gameMode == "pvc") {         //human v pc game mode
+        if(gameMode == "pvc") {         //human v pc game mode.....as of now bot is always yellow
             if(!blueTurn){
-                move = jarvis.getMove();   //bot makes move here
+                move = jarvis.getMove();   //bot makes move here...rest of this method is just animation (if you can call it that)
                 move[0].setSelected(true);      //"animation" of bot move starts here
                 try{                              //bots selected piece gets highlighted 
                     Thread.sleep(1000);    //the piece stays highlighted for a second for player to see
@@ -77,10 +74,24 @@ public class GUI {
             }
         }
         if(gameMode == "cvc") {         //This will work just like pvc but with two bots
-            return null;
+            if(!blueTurn){
+                //move = yellowBot.getMove();
+            } else {
+                //move = blueBot.getMove();
+            } //Now we animate the move
+            move[0].setSelected(true);      //"animation" of bot move starts here
+            try{                              //bots selected piece gets highlighted 
+                Thread.sleep(1000);    //the piece stays highlighted for a second for player to see
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            move[0].setSelected(false);
+            return move;
         }
-        return null;
+        return null;  //shouldn't be reached but there's no squiggles so its gonna stay
     }
+
+
     
     private static CheckerSquare[] getMoveFromMouse() { //this function kinda scuffed but it works for now...sometimes it seems glitchy in game....if any one has an idea of how to make better lmk
         CheckerSquareMouse.active = true;               //activates the Mouse adapter
