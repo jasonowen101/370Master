9bc0db2a28c1b0f2619932e016857be460901434
cwilson80 <cwilson80@murraystate.edu>
2023-11-20 15:43:41 -0600

    Update TeamB_AI.java

--- Team B/checkers/TeamB_AI.java
+++ Team B/checkers/TeamB_AI.java
@@ -11,12 +11,16 @@ public class TeamB_AI {
     private Color teamColor;
     
 
-    final static int KING_VALUE = 4;
-    final static int JUMP_VALUE = 3;
-    final static int MOVE_VALUE = 2;
+    final static int KING_VALUE = 5;
+    final static int JUMP_VALUE = 4;
+    final static int MOVE_VALUE = 3;
+    final static int UNSAFE_JUMP_VALUE = 2;
     final static int UNSAFE_MOVE_VALUE = 1;
 
-    // constructor for AI
+    /**
+     * Constructor for the AI.
+     * @param team The color of the team that the AI is on.
+     */
     public TeamB_AI(Color team) {
         if (team != null) {
             if (Color.BLUE.equals(team)) {
@@ -28,18 +32,22 @@ public class TeamB_AI {
                 teamColor = Color.YELLOW;
             }
         } else {
-            // Handle the case where team is null (provide default behavior or throw an exception)
+            // Handle the case where team is null (provide default behavior or throw an exception).
             throw new IllegalArgumentException("Team color cannot be null!");
         }
     }
 
+    /**
+     * Calculates the move with the highest score to perform.
+     * @return Length of 2 CheckerSquare array with the original and new positions a piece should move to.
+     */
     public CheckerSquare[] getMove() {
         boardState = GamePanel.getSquares();
         if(teamColor.equals(Color.YELLOW)) {
             boardState = flipBoard(boardState);
         }
 
-        // find the best potential move of each piece
+        // Find the best potential move of each piece.
         ArrayList<Move> potentialMoves = new ArrayList<>();
         for(CheckerSquare[] boardState1D : boardState) {
             for(CheckerSquare square : boardState1D) {
@@ -52,20 +60,17 @@ public class TeamB_AI {
             }
         }
 
-        // iterate through potential moves and select the highest score
+        // Iterate through potential moves and select the highest score.
         Move toPerform = potentialMoves.get(0);
         int score = -1;
         for(Move m : potentialMoves) {
             if(m.getScore() > score) {
                 toPerform = m;
                 score = m.getScore();
-                // if(potentialMoves.get(0).getScore() == potentialMoves.get(potentialMoves.size() - 1).getScore()) {
-                //     toPerform = potentialMoves.get(0);
-                // }
             }
         }
 
-        // print selected move for debugging purposes
+        // Print selected move for debugging purposes.
         System.out.println(toPerform.getScore());
         System.out.println("Row1: " + toPerform.getMovement()[0].getRow() + 
             " Col1: " + toPerform.getMovement()[0].getCol() + 
@@ -75,12 +80,22 @@ public class TeamB_AI {
         return toPerform.getMovement();
     }
 
-    // check if position is within bounds of board
+    /**
+     * Check if the position is within the bounds of the board.
+     * @param r The row of the position to be checked.
+     * @param c The column of the position to be checked.
+     * @return Whether or not the position is within bounds.
+     */
     private boolean validPos(int r, int c) {
         return r > -1 && r < 8 && c > -1 && c < 8;
     }
 
-    // check if position is an enemy
+    /**
+     * Check if the position is an enemy.
+     * @param r The row of the position to be checked.
+     * @param c The column of the position to be checked.
+     * @return Whether or not the position is an enemy.
+     */
     private boolean isEnemy(int r, int c) {
         if (validPos(r, c)) {
             Color checkerColor = boardState[r][c].getCheckerColor();
@@ -90,7 +105,12 @@ public class TeamB_AI {
         }
     }
 
-    // check if position is an enemy king
+    /**
+     * Check if the position is an enemy king.
+     * @param r The row of the position to be checked.
+     * @param c The column of the position to be checked.
+     * @return Whether or not the position is an enemy king.
+     */
     private boolean isEnemyKing(int r, int c) {
         if (validPos(r, c)) {
             Color checkerColor = boardState[r][c].getCheckerColor();
@@ -100,7 +120,12 @@ public class TeamB_AI {
         }
     }
 
-    // check if position is empty
+    /**
+     * Check if the position is empty.
+     * @param r The row of the position to be checked.
+     * @param c The column of the position to be checked.
+     * @return Whether or not the position is empty.
+     */
     private boolean isEmpty(int r, int c) {
         if (validPos(r, c)) {
             return Objects.isNull(boardState[r][c].getCheckerColor());
@@ -109,13 +134,26 @@ public class TeamB_AI {
         }
     }
 
-    // check if the position is a jump
+    /**
+     * Check if the position is a valid jump.
+     * @param r1 The row of the piece to be jumped.
+     * @param c1 The column of the piece to be jumped.
+     * @param r2 The row of the destination position.
+     * @param c2 The column of the destination postion.
+     * @return Whether or not the positions could be a jump.
+     */
     private boolean isJump(int r1, int c1, int r2, int c2) {
         return isEmpty(r2, c2) && validPos(r2, c2) && isEnemy(r1, c1);
     }
 
-    // check if position is safe. isLeft and isDown are for indicating the direction
-    // a piece is moving
+    /**
+     * Checks whether or not the specified postion is safe.
+     * @param r The row of the position to be checked.
+     * @param c The column of the position to be checked.
+     * @param originR The row that will become empty following the action.
+     * @param originC The column that will become empty following the action.
+     * @return Whether or not the new position is safe.
+     */
     private boolean isSafe(int r, int c, int originR, int originC) {
         return !((isEnemy(r - 1, c - 1) && (isEmpty(r + 1, c + 1) || ((r + 1 == originR) && (c + 1 == originC))))
                 || (isEnemy(r - 1, c + 1) && (isEmpty(r + 1, c - 1) || ((r + 1 == originR) && (c - 1 == originC))))
@@ -123,7 +161,12 @@ public class TeamB_AI {
                 || (isEnemyKing(r + 1, c + 1) && (isEmpty(r - 1, c - 1) || ((r - 1 == originR) && (c - 1 == originC)))));
     }
 
-    // check if position will king a piece
+    /**
+     * Check if the position would result in becoming a king.
+     * @param r The row of the position to be checked.
+     * @param c The column of the position to be checked.
+     * @return Whether or not the position would result in becoming a king.
+     */
     private boolean isKingSpace(int r, int c) {
         if (validPos(r, c))
             return isEmpty(r, c) && r == 7;
@@ -131,33 +174,48 @@ public class TeamB_AI {
             return false;
     }
 
-    // calculate all possible moves for a piece and return most valuable
+    /**
+     * Evaluates all possible moves a piece can make and assigns a score.
+     * It then picks the move that has the highest score and returns it.
+     * @param piece The piece to be evaluated.
+     * @return The move with the highest score.
+     */
     public Move valuate(CheckerSquare piece) {
         if (Objects.isNull(piece))
             throw new NullPointerException("Piece cannot be null!");
 
-        // create an ArrayList that stores the moves the piece can make
+        // Create an ArrayList that stores the moves the piece can make.
         ArrayList<Move> moves = new ArrayList<>();
         Move bestMove = new Move(new CheckerSquare[]{null, null}, -1);
 
-        // store row and collumn of piece, flip if needed
+        // Store row and collumn of piece, flip if needed.
         int r = piece.getRow();
         if(teamColor.equals(Color.YELLOW)) {
             r = 7 - r;
         }
         int c = piece.getCol();
 
-        // check if diagonal up-left is a jump
+        // Check if diagonal up-left is a jump.
         if (isJump(r - 1, c - 1, r - 2, c - 2)) {
-            moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 2][c - 2]}, JUMP_VALUE));
+            // Check if the end position is safe.
+            if(isSafe(r - 2, c - 2, r - 1, c - 1)) {
+                moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 2][c - 2]}, JUMP_VALUE));
+            } else {
+                moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 2][c - 2]}, UNSAFE_JUMP_VALUE));
+            }
         }
-        // check if diagonal up-right is a jump
+        // Check if diagonal up-right is a jump.
         if (isJump(r - 1, c + 1, r - 2, c + 2)) {
-            moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 2][c + 2]}, JUMP_VALUE));
+            // Check if the end position is safe.
+            if(isSafe(r - 2, c + 2, r - 1, c + 1)) {
+                moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 2][c + 2]}, JUMP_VALUE));
+            } else {
+                moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 2][c + 2]}, UNSAFE_JUMP_VALUE));
+            }
         }
-        // check if diagonal up-left is empty
+        // Check if diagonal up-left is empty.
         if (isEmpty(r - 1, c - 1)) {
-            // check if position results in a king, is safe, or is unsafe
+            // Check if position results in a king, is safe, or is unsafe.
             if (isKingSpace(r - 1, c - 1) && !piece.isKing()) {
                 moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 1][c - 1]}, KING_VALUE));
             } else if (isSafe(r - 1, c - 1, r, c)) {
@@ -166,9 +224,9 @@ public class TeamB_AI {
                 moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 1][c - 1]}, UNSAFE_MOVE_VALUE));
             }
         }
-        // check if diagonal up-right is empty
+        // Check if diagonal up-right is empty.
         if (isEmpty(r - 1, c + 1)) {
-            // check if position results in a king, is safe, or is unsafe
+            // Check if position results in a king, is safe, or is unsafe.
             if (isKingSpace(r - 1, c + 1) && !piece.isKing()) {
                 moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 1][c + 1]}, KING_VALUE));
             } else if (isSafe(r - 1, c + 1, r, c)) {
@@ -177,19 +235,29 @@ public class TeamB_AI {
                 moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r - 1][c + 1]}, UNSAFE_MOVE_VALUE));
             }
         }
-        // evaluate king specific moves
+        // Evaluate king specific moves.
         if (piece.isKing()) {
-            // check if diagonal down-left is a jump
+            // Check if diagonal down-left is a jump.
             if (isJump(r + 1, c - 1, r + 2, c - 2)) {
-                moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 2][c - 2]}, JUMP_VALUE));
+                // Check if the end position is safe.
+                if(isSafe(r + 2, c - 2, r + 1, c - 1)) {
+                    moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 2][c - 2]}, JUMP_VALUE));
+                } else {
+                    moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 2][c - 2]}, UNSAFE_JUMP_VALUE));
+                }
             }
-            // check if diagonal down-right is a jump
+            // Check if diagonal down-right is a jump.
             if (isJump(r + 1, c + 1, r + 2, c + 2)) {
-                moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 2][c + 2]}, JUMP_VALUE));
+                // Check if the end position is safe.
+                if(isSafe(r + 2, c + 2, r + 1, c + 1)) {
+                    moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 2][c + 2]}, JUMP_VALUE));
+                } else {
+                    moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 2][c + 2]}, UNSAFE_JUMP_VALUE));
+                }
             }
-            // check if diagonal down-left is empty
+            // Check if diagonal down-left is empty.
             if (isEmpty(r + 1, c - 1)) {
-                // check if position is safe
+                // Check if position is safe.
                 if (isSafe(r + 1, c - 1, r, c)) {
                     moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 1][c - 1]}, MOVE_VALUE));
                 } else {
@@ -197,9 +265,9 @@ public class TeamB_AI {
                             UNSAFE_MOVE_VALUE));
                 }
             }
-            // check if diagonal down-right is empty
+            // Check if diagonal down-right is empty.
             if (isEmpty(r + 1, c + 1)) {
-                // check if position is safe
+                // Check if position is safe.
                 if (isSafe(r + 1, c + 1, r, c)) {
                     moves.add(new Move(new CheckerSquare[]{boardState[r][c], boardState[r + 1][c + 1]}, MOVE_VALUE));
                 } else {
@@ -209,7 +277,7 @@ public class TeamB_AI {
             }
         }
 
-        // iterate through potential moves and select the highest score
+        // Iterate through potential moves and select the highest score.
         int s = -1;
         for (Move m : moves) {
             if (m.getScore() > s) {
@@ -221,11 +289,17 @@ public class TeamB_AI {
         return bestMove;
     }
 
-    // vertically flips the supplied CheckerSquare[][] and returns it
+    /**
+     * Vertically flips the supplied 2D array of CheckerSquares.
+     * @param board The 2D array of CheckerSquares to be flipped.
+     * @return The now flipped 2D array of CheckerSquares.
+     */
     private CheckerSquare[][] flipBoard(CheckerSquare[][] board) {
+        // Store board length to reduce calls to board.length.
         int length = board.length;
         CheckerSquare[][] flippedBoard = new CheckerSquare[length][length];
 
+        // Iterate through the board and vertically flip it.
         for (int r = 0; r < length; r++) {
             for (int c = 0; c < length; c++) {
                 flippedBoard[(length - 1) - r][c] = board[r][c];
